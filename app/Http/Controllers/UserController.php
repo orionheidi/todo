a66457e40cd6998b66b40162795a4373c8545f8a
orionheidi <34884510+orionheidi@users.noreply.github.com>
2022-05-21 17:14:32 +0200

    send email notification for done tasks

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,28 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\UserCollection;
-use App\Models\Task;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
 
 class UserController extends Controller
 {
     public function index()
     {
         return new UserCollection(User::all());
-    }
-
-    public function sendUserEmailNotification(Request $request, $listId)
-    {
-        $user = $request->user();
-
-        $tasks[] = Task::where('daily_list_id', $listId)->where('updated_at',  '>=',  Carbon::today()->toDateString())->
-            where('done',  true)->get();
-
-        return response()->json([
-            'message' => 'Tasks done today.',
-            'filtered tasks' => $tasks,
-        ]);
     }
 }
